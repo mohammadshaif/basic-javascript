b08f5ff79c58ea0a4d859e82330d22b5c986722c
mohammadshaif <mohammadeshaif27@gmail.com>
2020-04-21 09:22:26 +0600

    some array

--- js problems/array duplicate.js
+++ js problems/array duplicate.js
@@ -1,16 +1,3 @@
-// var stroll = [32, 60, 50,85 ,90,32 ,45, 85];
-// var newroll = [];
-// for (let i = 0; i < stroll.length; i++) {
-//     var element = stroll[i];
-//     var index = newroll.indexOf(element);
-//     if (index == -1) {
-//         newroll.push(element)
-        
-//     }
-    
-// }
-// console.log(newroll)
-
 var stroll = [32, 60, 50,85 ,90,32 ,45, 85];
 var newroll =[];
 for (let i = 0; i < stroll.length; i++) {
@@ -25,7 +12,9 @@ console.log(newroll)
 
 //es6
 var stroll = [32, 60, 50,85 ,90,32 ,45, 85];
-var dub = new Set(stroll)
-var backToArray = [...dub]
-console.log(backToArray);
+// var dub = new Set(stroll)
+// var backToArray = [...dub]
+// console.log(backToArray);
+var newArray = Array.from(new Set(stroll))
+console.log(newArray);
 
